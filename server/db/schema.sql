aad11dba1868d7ff4390fdcd062dcbd37fe89a0d
minseokchoi <mschoi16@uw.edu>
2021-05-30 19:18:41 +0900

    added settings for dockerr for database

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,26 +1,40 @@
 create table if not exists users (
-    id          int not null auto_increment primary key,
+    id          int not null auto_increment,
     email       varchar(225) not null unique,
     username    varchar(225) not null unique,
     passhash    binary(64) not null,
     first_name  varchar(128) not null,
     last_name   varchar(128) not null,
-    -- store_id    int not null references stores(id)
+    primary key(id)
 );
 
+-- store_id    int not null references stores(id)
 -- Updates: moved store_id because one user can have multiple stores
 
 create table if not exists stores (
-    id          int not null auto_increment primary key,
-    user_id     int not null references users(id),
+    id          int not null auto_increment,
+    user_id int not null,
     store_name       varchar(225) not null,
-    store_location     varchar(225) not null
+    store_location     varchar(225) not null,
+    primary key(id),
+    foreign key(user_id) references users(id)
+);
+
+create table if not exists items (
+    id          int not null auto_increment,
+    store_id int not null,
+    user_id     int not null,
+    item_name   varchar(225) not null,
+    price       int not null,
+    primary key(id),
+    foreign key(store_id) references stores(id),
+    foreign key(user_id) references users(id)
 );
 
 create table if not exists customers (
-    id          int not null auto_increment primary key,
-    user_id     int not null references users(id),
-    store_id    int not null references stores(id),
+    id          int not null auto_increment,
+    user_id int not null,
+    store_id int not null,
     first_name  varchar(128) not null,
     last_name   varchar(128) not null,
     ethnicity   varchar(225) not null,
@@ -29,14 +43,20 @@ create table if not exists customers (
     postal_code int not null,
     last_visited    date not null,
     dis_channel varchar(225) not null,
-    fav_item    int references items(id)
+    fav_item    int not null,
+    primary key(id),
+    foreign key(user_id) references users(id),
+    foreign key(store_id) references stores (id),
+    foreign key(fav_item) references items (id)
 );
 
-create table if not exists items (
-    id          int not null auto_increment primary key,
-    store_id    int not null references stores(id),
-    item_name   varchar(225) not null.
-    price       int not null
+create table if not exists UserSignIn (
+    UserID varchar(20) not null,
+    SignInTime datetime not null,
+    ClientIP varchar(255) not null
 );
 
-/* TODO: user sign in log schema 
+
+
+
+-- TODO: user sign in log schema 
