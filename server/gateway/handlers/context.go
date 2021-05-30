059a9e97fae56e0038043402be46670f75c8a505
minseokchoi <mschoi16@uw.edu>
2021-05-30 19:22:09 +0900

    added different storage to handler

--- server/gateway/handlers/context.go
+++ server/gateway/handlers/context.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
-	"final-project/server/gateway/models/customers"
-	"final-project/server/gateway/models/users"
-	"final-project/server/gateway/sessions"
+	"github.com/info441-sp21/final-project/server/gateway/models/customers"
+	"github.com/info441-sp21/final-project/server/gateway/models/users"
+
+	"github.com/info441-sp21/final-project/server/gateway/sessions"
+
+	"github.com/info441-sp21/final-project/server/gateway/models/items"
+	"github.com/info441-sp21/final-project/server/gateway/models/stores"
 )
 
 type HttpHandler struct {
@@ -11,4 +15,6 @@ type HttpHandler struct {
 	SessionStore    sessions.Store
 	UserStorage     users.Storage
 	CustomerStorage customers.Storage
+	ItemStorage     items.Storage
+	StoreStorage    stores.Storage
 }
