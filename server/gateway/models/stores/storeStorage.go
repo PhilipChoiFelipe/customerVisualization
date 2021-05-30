c158a6b8d9cc217aff45ec480c3fbd9841ae3d30
minseokchoi <mschoi16@uw.edu>
2021-05-30 19:29:50 +0900

    fixed typo bug and added DeleteAllbyUserId

--- server/gateway/models/stores/storeStorage.go
+++ server/gateway/models/stores/storeStorage.go
@@ -9,15 +9,22 @@ type StoreStorage struct {
 	DB *sql.DB
 }
 
+func NewSqlStorage(sqlsess *sql.DB) *StoreStorage {
+	if sqlsess == nil {
+		panic("nil sql session")
+	}
+	return &StoreStorage{sqlsess}
+}
+
 var (
-	id        int64
-	user_id 	int64
-	store_name string
+	id             int64
+	user_id        int64
+	store_name     string
 	store_location string
 )
 
 //GetByID returns the store with the given store ID
-func (ss *StoreStorage) GetByID(id int64) (*Store, error) {
+func (ss *StoreStorage) GetById(id int64) (*Store, error) {
 	row := ss.DB.QueryRow("select * from stores where id=?", id)
 	store := &Store{}
 	if err := row.Scan(&store.ID, &store.UserID, &store.StoreName, &store.StoreLocation); err != nil {
@@ -28,7 +35,7 @@ func (ss *StoreStorage) GetByID(id int64) (*Store, error) {
 
 //GetStores returns all stores of given user ID
 func (ss *StoreStorage) GetStores(userId int64) ([]*Store, error) {
-	rows, err := ss.DB.Query("select * from stores where id=?", id)
+	rows, err := ss.DB.Query("select * from stores where user_id=?", userId)
 	if err != nil {
 		return nil, fmt.Errorf("%v: %v", ErrStoreNotFound, err)
 	}
@@ -49,9 +56,9 @@ func (ss *StoreStorage) GetStores(userId int64) ([]*Store, error) {
 
 //Insert new stores and returns inserted store
 func (ss *StoreStorage) Insert(store *Store) (*Store, error) {
-	insq := "insert into stores(store_id, user_id, store_name, price) values (?,?,?,?)"
+	insq := "insert into stores(user_id, store_name, store_location) values (?,?,?)"
 
-	res, err := ss.DB.Exec(insq, store.ID, store.UserID, store.StoreName, store.StoreLocation)
+	res, err := ss.DB.Exec(insq, store.UserID, store.StoreName, store.StoreLocation)
 	if err != nil {
 		return nil, fmt.Errorf("%v: %v", ErrStoreNotInserted, err)
 	}
@@ -78,7 +85,7 @@ func (ss *StoreStorage) Update(id int64, updates *StoreUpdate) (*Store, error) {
 		return nil, fmt.Errorf("%v: %v", ErrStoreNotUpdated, err)
 	}
 
-	return ss.GetByID(id)
+	return ss.GetById(id)
 }
 
 //Delete deletes store with given ID
@@ -89,5 +96,15 @@ func (ss *StoreStorage) Delete(id int64) error {
 		return fmt.Errorf("%v: %v", ErrStoreNotDeleted, err)
 	}
 
+	return nil
+}
+
+//DeleteAllbyUserId deletes every stores related to user
+func (ss *StoreStorage) DeleteAllbyUserId(userId int64) error {
+	delq := "delete from items where user_id = ?"
+	_, err := ss.DB.Exec(delq, userId)
+	if err != nil {
+		return fmt.Errorf("%v: %v", ErrStoreNotDeleted, err)
+	}
 	return nil
 }
